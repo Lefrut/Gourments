6937201a17487a599c12be35fc0c1bd4c3a2f697
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-05-13 16:16:02 +0300

    Custom shapes has been created

---
+++ app/src/main/java/com/dashkevich/gourments/ui/theme/Shape.kt
@@ -0,0 +1,8 @@
+package com.dashkevich.gourments.ui.theme
+
+import androidx.compose.ui.unit.dp
+import com.dashkevich.gourments.ui.theme.model.GourmentsShape
+
+val GourmentsShape = GourmentsShape(
+    default = 8.dp
+)
---
+++ app/src/main/java/com/dashkevich/gourments/ui/theme/model/GourmentsShape.kt
@@ -0,0 +1,7 @@
+package com.dashkevich.gourments.ui.theme.model
+
+import androidx.compose.ui.unit.Dp
+
+data class GourmentsShape(
+    val default: Dp
+)
