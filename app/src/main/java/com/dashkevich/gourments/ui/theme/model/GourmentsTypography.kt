efbc452356159cb14e6bda64a87361753773bd2d
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-05-13 16:17:28 +0300

    Custom fonts have been created

---
+++ app/src/main/java/com/dashkevich/gourments/ui/theme/model/GourmentsTypography.kt
@@ -0,0 +1,16 @@
+package com.dashkevich.gourments.ui.theme.model
+
+import androidx.compose.ui.text.TextStyle
+
+data class GourmentsTypography(
+    val button: TextStyle,
+    val body2: TextStyle,
+    val body2Secondary: TextStyle,
+    val h4: TextStyle,
+    val body1: TextStyle,
+    val h18: TextStyle,
+    val body1MediumSecondary: TextStyle,
+    val body1Medium: TextStyle,
+    val defaultRegularBody: TextStyle,
+    val defaultBoldCaption2: TextStyle
+)
