1813eb7763b305375ad9357bb62ae8d1bb948e8f
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-05-17 16:37:52 +0300

    processing ViewModel categories

--- app/src/main/java/com/dashkevich/gourmets/di/other/ViewModelModules.kt
+++ app/src/main/java/com/dashkevich/gourmets/di/other/ViewModelModules.kt
@@ -1,12 +1,14 @@
 package com.dashkevich.gourmets.di.other
 
+import com.dashkevich.gourmets.data.domain.repository.GourmetsRepository
+import com.dashkevich.gourmets.data.domain.repository.GourmetsRepositoryImpl
 import com.dashkevich.gourmets.ui.screens.catalog.CatalogViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModules = module {
-    viewModel{
-        CatalogViewModel()
+    viewModel {
+        CatalogViewModel(gourmetsRepository = get())
     }
 
 }
--- app/src/main/java/com/dashkevich/gourmets/ui/screens/catalog/СatalogViewModel.kt
+++ app/src/main/java/com/dashkevich/gourmets/ui/screens/catalog/СatalogViewModel.kt
@@ -1,19 +1,42 @@
 package com.dashkevich.gourmets.ui.screens.catalog
 
+import androidx.lifecycle.viewModelScope
+import com.dashkevich.gourmets.common.OperationState
 import com.dashkevich.gourmets.common.mvi.BaseViewModel
+import com.dashkevich.gourmets.data.domain.repository.GourmetsRepository
 import com.dashkevich.gourmets.ui.screens.catalog.model.mvi.CatalogIntent
 import com.dashkevich.gourmets.ui.screens.catalog.model.mvi.CatalogState
+import com.dashkevich.gourmets.ui.screens.catalog.model.mvi.CategoriesTab
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class CatalogViewModel(): BaseViewModel<CatalogState,CatalogIntent>() {
+class CatalogViewModel(
+    private val gourmetsRepository: GourmetsRepository
+) : BaseViewModel<CatalogState, CatalogIntent>() {
     private val _viewState = MutableStateFlow(CatalogState())
     override val viewState: StateFlow<CatalogState> = _viewState.asStateFlow()
 
     override fun processingIntent(intent: CatalogIntent) {
-        when(intent){
-            else -> { }
+        when (intent) {
+            else -> {}
+        }
+    }
+
+    private fun getCategories() = viewModelScope.launch {
+        _viewState.value =
+            viewState.value.copy(categoriesTab = CategoriesTab(state = OperationState.Loading))
+        gourmetsRepository.getCategories().onSuccess { categories ->
+            val state = if (categories.isEmpty()) OperationState.EmptyResult
+            else OperationState.Success
+            _viewState.value = viewState.value.copy(
+                categoriesTab = CategoriesTab(categories, state)
+            )
+        }.onFailure {
+            _viewState.value = viewState.value.copy(
+                categoriesTab = CategoriesTab(state = OperationState.Error)
+            )
         }
     }
 
