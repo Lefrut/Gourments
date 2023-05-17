f091e58f1e60a06d0d20fd8b6e9ed7d5e649873f
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-05-17 15:23:17 +0300

    created a gourmets repository - 1 request for categories was made from the repository

---
+++ app/src/main/java/com/dashkevich/gourmets/data/domain/repository/GourmetsRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.dashkevich.gourmets.data.domain.repository
+
+import com.dashkevich.gourmets.data.api.GourmetsService
+import com.dashkevich.gourmets.data.api.model.Category
+import com.dashkevich.gourmets.data.util.coroutineCatching
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+class GourmetsRepositoryImpl(
+    private val gourmetsService: GourmetsService,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+): GourmetsRepository {
+
+    override suspend fun getCategories()
+    : Result<List<Category>> = coroutineCatching(dispatcher) {
+        gourmetsService.getCategories()
+    }
+
+}
