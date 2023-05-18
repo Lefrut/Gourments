28bc20d3574f7f9f0d1d22991b32c0f8825d61d3
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-05-18 16:48:44 +0300

    create repository product request

--- app/src/main/java/com/dashkevich/gourmets/data/domain/repository/GourmetsRepository.kt
+++ app/src/main/java/com/dashkevich/gourmets/data/domain/repository/GourmetsRepository.kt
@@ -1,9 +1,12 @@
 package com.dashkevich.gourmets.data.domain.repository
 
 import com.dashkevich.gourmets.data.api.model.Category
+import com.dashkevich.gourmets.data.api.model.Product
 
 interface GourmetsRepository {
 
     suspend fun getCategories() : Result<List<Category>>
+
+    suspend fun getProduct(id: Int) : Result<Product>
 
 }
--- app/src/main/java/com/dashkevich/gourmets/data/domain/repository/GourmetsRepositoryImpl.kt
+++ app/src/main/java/com/dashkevich/gourmets/data/domain/repository/GourmetsRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.dashkevich.gourmets.data.domain.repository
 
 import com.dashkevich.gourmets.data.api.GourmetsService
 import com.dashkevich.gourmets.data.api.model.Category
+import com.dashkevich.gourmets.data.api.model.Product
 import com.dashkevich.gourmets.data.util.coroutineCatching
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -9,11 +10,16 @@ import kotlinx.coroutines.Dispatchers
 class GourmetsRepositoryImpl(
     private val gourmetsService: GourmetsService,
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
-): GourmetsRepository {
+) : GourmetsRepository {
 
     override suspend fun getCategories()
-    : Result<List<Category>> = coroutineCatching(dispatcher) {
+            : Result<List<Category>> = coroutineCatching(dispatcher) {
         gourmetsService.getCategories()
+    }
+
+    override suspend fun getProduct(id: Int)
+            : Result<Product> = coroutineCatching(dispatcher) {
+        gourmetsService.getProduct(id)
     }
 
 }
